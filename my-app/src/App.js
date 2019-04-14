c26e9ebc9bbce62605507f7301dcdbe7787dd4f9
suryabaira97 <surya@Surya-BD-C02S576EG8WN.local>
2019-04-13 23:11:15 -0500

    routing for react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,35 +1,29 @@
 import React, { Component } from 'react';
-import {Route,Switch,withRouter} from 'react-router-dom';
+import {BrowserRouter,Route,Switch,withRouter} from 'react-router-dom';
 import {connect,Provider} from 'react-redux';
 import {ConnectedRouter} from 'react-router-redux';
 import axios from 'axios';
 import logo from './logo.svg';
-// import Main from './pages/Main.js'
+import Main from './Pages/Main'
+import Landing from './Pages/LandingPage'
+import NoPage from './Pages/NoPage'
 import store from "./js/store/index";
 import './App.css';
 
 class App extends Component {
   render() {
-    // const Main =() => <Main/>;
+    // const Main =() => <Main/>;/
     return (
       <div style={{height:'100%'}}>
       <Provider store={store}>
-        <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+      <Switch>
+      <Route path="/" exact component = {Main}></Route>
+      <Route path="/Landing" exact component = {Landing}></Route>
+      <Route path="/NoPage" exact component = {NoPage}></Route>
+      </Switch>
+      </BrowserRouter>
+      
       </Provider>
 
       </div>
--- my-app/src/Pages/Main.js
+++ my-app/src/Pages/Main.js
@@ -3,11 +3,13 @@ import {Route,Switch,withRouter} from 'react-router-dom';
 import {connect,provider} from 'react-redux';
 import {ConnectedRouter} from 'react-router-redux';
 import axios from 'axios';
+import logo from '../logo.svg';
+import '../App.css';
 
 class Main extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
