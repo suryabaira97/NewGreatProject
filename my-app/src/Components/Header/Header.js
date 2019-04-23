49a3ec6694eab418aca91b42d21133cd09d6ffbf
suryabaira97 <surya@Surya-BD-C02S576EG8WN.local>
2019-04-23 16:17:21 -0500

    header changes

--- my-app/src/Components/Header/Header.js
+++ my-app/src/Components/Header/Header.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {Route,Switch,withRouter} from 'react-router-dom';
 import {connect,provider} from 'react-redux';
 import './Header.css';
-import { Home, ThreeDRotation ,More} from '@material-ui/icons';
+import { Home,Search ,ThreeDRotation ,More} from '@material-ui/icons';
 
 class Header extends Component {
   render() {
@@ -10,7 +10,11 @@ class Header extends Component {
       <div className="header">
         <header className="appHeader">
         <Home className="homeIcon"></Home>
-        <More className="MoreIcon"></More>
+        <div className="search-field">
+        <Search className="searchIcon"></Search>
+        <input className="search"></input>
+        </div>
+        {/* <More className="MoreIcon"></More> */}
         </header>
       </div>
     );
